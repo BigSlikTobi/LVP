7181a90705a9d4a040313fcec580561215e15674
BigSlikTobi <tobias.latta@hotmail.de>
2025-01-06 19:50:02 +0100

    Initial Commit

---
+++ LVP/LVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVPApp.swift
+//  LVP
+//
+//  Created by Tobias Latta on 06.01.25.
+//
+
+import SwiftUI
+
+@main
+struct LVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LVPTests/LVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  LVPTests.swift
+//  LVPTests
+//
+//  Created by Tobias Latta on 06.01.25.
+//
+
+import Testing
+@testable import LVP
+
+struct LVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
